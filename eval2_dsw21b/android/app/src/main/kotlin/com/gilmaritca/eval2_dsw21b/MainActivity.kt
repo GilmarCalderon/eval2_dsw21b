94c691a5a09416ce58f17fe17ba668081d3d2c29
diegojosue2 <gilmarcalderon89@gmail.com>
2023-10-03 01:55:15 -0600

    subida de proyecto

--- eval2_dsw21b/android/app/src/main/kotlin/com/gilmaritca/eval2_dsw21b/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.gilmaritca.eval2_dsw21b
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ evalu2_dsw21b/android/app/src/main/kotlin/com/gilmaritca/evalu2_dsw21b/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gilmaritca.evalu2_dsw21b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
